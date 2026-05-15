b2693956495aed8cc1282e41b91595888fabcf2d
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-05-15 10:33:35 +0200

    Update examples for v1.74.2

--- cookbook-aws-lambda/src/images-handler.js
+++ cookbook-aws-lambda/src/images-handler.js
@@ -1,50 +1,51 @@
-const AWS = require("aws-sdk");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+} = require("@aws-sdk/lib-dynamodb");
 const { v4: uuidv4 } = require("uuid");
 
-const imagesDB = new AWS.DynamoDB.DocumentClient();
+const dynamoDBClient = new DynamoDBClient({});
+const dynamoDBDocClient = DynamoDBDocumentClient.from(dynamoDBClient);
 
 const tableName = process.env.TABLE_NAME;
 
 exports.main = async function (event) {
-  const method = event.httpMethod;
-
   const routeKey = `${event.httpMethod} ${event.resource}`;
 
   try {
     switch (routeKey) {
       case "POST /images":
         const id = uuidv4();
-        const filename = `inspirational-quote-${id}.png`;
-
+        const filename = `awesome-headline-${id}.png`;
         const requestBody = JSON.parse(event.body);
-
-        const result = await imagesDB
-          .put({
-            TableName: tableName,
-            Item: {
-              id,
-              filename,
-              interpolationParams: JSON.stringify({
-                quote: requestBody.quote,
-              }),
-              creationStatus: "PENDING",
-              url: "",
-            },
-          })
-          .promise();
-
+        // Create a new item in the DB table
+        const putCommand = new PutCommand({
+          TableName: tableName,
+          Item: {
+            id,
+            filename,
+            interpolationParams: JSON.stringify({
+              headline: requestBody.headline,
+            }),
+            creationStatus: "PENDING",
+            url: "",
+          },
+        });
+        await dynamoDBDocClient.send(putCommand);
         var body = { id };
-
         break;
+
       case "GET /images/{id}":
-        body = await imagesDB
-          .get({
-            TableName: tableName,
-            Key: {
-              id: event.pathParameters.id,
-            },
-          })
-          .promise();
+        const getCommand = new GetCommand({
+          TableName: tableName,
+          Key: {
+            id: event.pathParameters.id,
+          },
+        });
+        const getResponse = await dynamoDBDocClient.send(getCommand);
+        body = getResponse.Item;
         break;
     }
 
